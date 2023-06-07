b46ea78e3c7fecbd0fdfd784527d64efb138dffc
Sidney François <sidney.fvm.dev@outlook.com>
2023-06-07 15:42:56 -0300

    feat: new basic Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+
+// import { Navbar } from "@/components/Navbar.vue";
+</script>
 <template>
   <div>
+    <Navbar />
     <h1>Hello World from road to tiny url!</h1>
   </div>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+  <header>
+    <img src="@/assets/logo-versions/logo-color.svg" alt="" />
+    <nav>
+      <ul>
+        <li>My tiny URLs</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<style scoped></style>
